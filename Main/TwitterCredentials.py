42e56daa43ca976d9a64243cd07da7c4ed1f8ef2
Chong Lok <chonglok@hotmail.co.uk>
2020-11-23 20:43:22 +0000

    Initial comit

--- Main/TwitterCredentials.py
+++ Main/TwitterCredentials.py
@@ -1,4 +1,4 @@
-consumer_key = "brEOF6rRaMhbn28ECFzoLmDKy"
-consumer_secret = "35V7ewsjFUfKMKIdbodizVLPNyYipd6NoLMUt7x2boLgtlcwza"
-access_key = "94837292-wwadjkK5gkzvAni99FHrA34a9GjfU6o9F17ARYCLm"
-access_secret = "eP1Ic37Ny4CVuInCTSpVu0tazo6GyUTH9iB7Br6UsIGHr"
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
